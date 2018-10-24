b1983fb7f894494e8eb0494922c64ee00d460e73
1156939681 <gcxgg0822@gamil.com>
2018-10-24 23:27:19 +0800

    add new

---
+++ src/GitTest.java
@@ -0,0 +1,12 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by guocui on 2018/10/24.
+ */
+public class GitTest {
+    private int age ;
+
+    public int getAge() {
+        return age;
+    }
+}
